a10918055fb76d91ccd1f9f850cfb41d2169c73e
Adam Pritchard <pritchard.adam@gmail.com>
2015-04-26 20:56:11 -0400

    Added browserify build

--- dist/markdown-it-headinganchor.js
+++ dist/markdown-it-headinganchor.js
@@ -1,4 +1,4 @@
-/*! markdown-it-headinganchor 1.1.1 https://github.com//adam-p/markdown-it-headinganchor @license MIT */(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.markdownitHeadingAnchor = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+/*! markdown-it-headinganchor 1.2.0 https://github.com//adam-p/markdown-it-headinganchor @license MIT */(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.markdownitHeadingAnchor = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /*
  * Copyright Adam Pritchard 2015
  * MIT License : http://adampritchard.mit-license.org/
@@ -17,21 +17,30 @@ function makeRule(options) {
         continue;
       }
 
+      var headingOpenToken = state.tokens[i+1];
       var headingInlineToken = state.tokens[i+1];
 
       if (!headingInlineToken.content) {
         continue;
       }
 
-      var anchorToken = new state.Token('html_inline', '', 0);
-      anchorToken.content =
-        '<a name="' +
-        encodeURIComponent(headingInlineToken.content) +
-        '" class="' +
-        options.anchorClass +
-        '" href="#"></a>';
+      var anchorName = encodeURIComponent(headingInlineToken.content);
 
-      headingInlineToken.children.unshift(anchorToken);
+      if (options.addHeadingID) {
+        state.tokens[i].attrPush(['id', anchorName]);
+      }
+
+      if (options.addHeadingAnchor) {
+        var anchorToken = new state.Token('html_inline', '', 0);
+        anchorToken.content =
+          '<a name="' +
+          anchorName +
+          '" class="' +
+          options.anchorClass +
+          '" href="#"></a>';
+
+        headingInlineToken.children.unshift(anchorToken);
+      }
 
       // Advance past the inline and heading_close tokens.
       i += 2;
@@ -41,7 +50,9 @@ function makeRule(options) {
 
 module.exports = function headinganchor_plugin(md, opts) {
   var defaults = {
-    anchorClass: 'markdown-it-headinganchor'
+    anchorClass: 'markdown-it-headinganchor',
+    addHeadingID: true,
+    addHeadingAnchor: true
   };
   var options = md.utils.assign(defaults, opts);
   md.core.ruler.push('heading_anchors', makeRule(options));
